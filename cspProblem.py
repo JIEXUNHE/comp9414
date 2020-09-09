752a5c38f5439cd03ae3bdb39b920d6618056294
Matchman <33885018+JIEXUNHE@users.noreply.github.com>
2020-09-09 11:57:05 +0800

    Add files via upload

---
+++ cspProblem.py
@@ -0,0 +1,66 @@
+# cspProblem.py - Representations of a Constraint Satisfaction Problem
+# AIFCA Python3 code Version 0.8.1 Documentation at http://aipython.org
+
+# Artificial Intelligence: Foundations of Computational Agents
+# http://artint.info
+# Copyright David L Poole and Alan K Mackworth 2017.
+# This work is licensed under a Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# See: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
+
+class Constraint(object):
+    """A Constraint consists of
+    * scope: a tuple of variables
+    * condition: a function that can applied to a tuple of values
+    for the variables
+    """
+    def __init__(self, scope, condition):
+        self.scope = scope
+        self.condition = condition
+
+    def __repr__(self):
+        return self.condition.__name__ + str(self.scope)
+
+    def holds(self,assignment):
+        """returns the value of Constraint con evaluated in assignment.
+
+        precondition: all variables are assigned in assignment
+        """
+        return self.condition(*tuple(assignment[v] for v in self.scope))
+
+class CSP(object):
+    """A CSP consists of
+    * domains, a dictionary that maps each variable to its domain
+    * constraints, a list of constraints
+    * variables, a set of variables
+    * var_to_const, a variable to set of constraints dictionary
+    """
+    def __init__(self,domains,constraints):
+        """domains is a variable:domain dictionary
+        constraints is a list of constriants
+        """
+        self.variables = set(domains)
+        self.domains = domains
+        self.constraints = constraints
+        self.var_to_const = {var:set() for var in self.variables}
+        for con in constraints:
+            for var in con.scope:
+                self.var_to_const[var].add(con)
+
+    def __str__(self):
+        """string representation of CSP"""
+        return str(self.domains)
+
+    def __repr__(self):
+        """more detailed string representation of CSP"""
+        return "CSP("+str(self.domains)+", "+str([str(c) for c in self.constraints])+")"
+
+    def consistent(self,assignment):
+        """assignment is a variable:value dictionary
+        returns True if all of the constraints that can be evaluated
+                        evaluate to True given assignment.
+        """
+        return all(con.holds(assignment)
+                    for con in self.constraints
+                    if all(v in  assignment  for v in con.scope))
+
---
+++ cspSearch.py
@@ -0,0 +1,72 @@
+# cspSearch.py - Representations of a Search Problem from a CSP.
+# AIFCA Python3 code Version 0.8.1 Documentation at http://aipython.org
+
+# Artificial Intelligence: Foundations of Computational Agents
+# http://artint.info
+# Copyright David L Poole and Alan K Mackworth 2017.
+# This work is licensed under a Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# See: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
+
+from cspProblem import CSP, Constraint
+from searchProblem import Arc, Search_problem
+from utilities import dict_union
+
+class Search_from_CSP(Search_problem):
+    """A search problem directly from the CSP.
+
+    A node is a variable:value dictionary"""
+    def __init__(self, csp, variable_order=None):
+        self.csp=csp
+        if variable_order:
+            assert set(variable_order) == set(csp.variables)
+            assert len(variable_order) == len(csp.variables)
+            self.variables = variable_order
+        else:
+            self.variables = list(csp.variables)
+
+    def is_goal(self, node):
+        """returns whether the current node is a goal for the search
+        """
+        return len(node)==len(self.csp.variables)
+    
+    def start_node(self):
+        """returns the start node for the search
+        """
+        return {}
+    
+    def neighbors(self, node):
+        """returns a list of the neighboring nodes of node.
+        """
+        var = self.variables[len(node)] # the next variable
+        res = []
+        for val in self.csp.domains[var]:
+            new_env = dict_union(node,{var:val})  #dictionary union
+            if self.csp.consistent(new_env):
+                res.append(Arc(node,new_env))
+        return res
+
+from cspExamples import csp1,csp2,test, crossword1, crossword1d
+from searchGeneric import Searcher
+
+def dfs_solver(csp):
+    """depth-first search solver"""
+    path = Searcher(Search_from_CSP(csp)).search()
+    if path is not None:
+        return path.end()
+    else:
+        return None
+
+if __name__ == "__main__":
+    test(dfs_solver)
+
+## Test Solving CSPs with Search:
+searcher1 = Searcher(Search_from_CSP(csp1))
+#print(searcher1.search())  # get next solution
+searcher2 = Searcher(Search_from_CSP(csp2))
+#print(searcher2.search())  # get next solution
+searcher3 = Searcher(Search_from_CSP(crossword1))
+#print(searcher3.search())  # get next solution
+searcher4 = Searcher(Search_from_CSP(crossword1d))
+#print(searcher4.search())  # get next solution (warning: slow)
+
